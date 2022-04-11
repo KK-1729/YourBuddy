26c718aab6dbcb3834aff0bb88a08c20c13e2832
ritiknaidu <ritiknaidu12@gmail.com>
2022-04-11 12:44:25 +0530

    added NavBar struct

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div>
+            <h1>NavBar coming soon!</h1>
+        </div>
+    );
+}
+
+export default NavBar;
